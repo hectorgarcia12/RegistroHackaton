3b6d1b647c104fc99222d8c1e2baa66adcc5ce15
Héctor García <htr.garcia12@gmail.com>
2019-03-22 19:05:42 -0600

    Se modifica html y scss de participantes

---
+++ src/app/pages/participante/participante.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-participante',
+  templateUrl: './participante.component.html',
+  styleUrls: ['./participante.component.scss']
+})
+export class ParticipanteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
